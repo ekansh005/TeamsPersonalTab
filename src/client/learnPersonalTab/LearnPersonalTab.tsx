5d1d85866b2cf061da6efdf866c989b3073ad2d9
Ekansh Saxena <ekansh.a.saxena@capgemini.com>
2022-01-14 19:57:06 +0200

    Add to do functionality

--- src/client/learnPersonalTab/LearnPersonalTab.tsx
+++ src/client/learnPersonalTab/LearnPersonalTab.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
-import { Provider, Flex, Text, Button, Header } from "@fluentui/react-northstar";
+import {
+    Provider, Flex, Text, Button, Header,
+    List, Alert, WindowMaximizeIcon, ExclamationTriangleIcon,
+    Label, Input, ToDoListIcon
+} from "@fluentui/react-northstar";
 import { useState, useEffect } from "react";
 import { useTeams } from "msteams-react-base-component";
 import * as microsoftTeams from "@microsoft/teams-js";
@@ -11,6 +15,8 @@ export const LearnPersonalTab = () => {
 
     const [{ inTeams, theme, context }] = useTeams();
     const [entityId, setEntityId] = useState<string | undefined>();
+    const [todoItems, setTodoItems] = useState<string[]>(["Submit time sheet", "Submit expense report"]);
+    const [newTodoValue, setNewTodoValue] = useState<string>("");
 
     useEffect(() => {
         if (inTeams === true) {
@@ -26,33 +32,56 @@ export const LearnPersonalTab = () => {
         }
     }, [context]);
 
+    const handleOnChanged = (event): void => {
+        setNewTodoValue(event.target.value);
+    };
+
+    const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
+        const newTodoItems = todoItems;
+        newTodoItems.push(newTodoValue);
+        setTodoItems(newTodoItems);
+        setNewTodoValue("");
+    };
+
     /**
      * The render() method to create the UI of the tab
      */
     return (
         <Provider theme={theme}>
-            <Flex fill={true} column styles={{
-                padding: ".8rem 0 .8rem .5rem"
-            }}>
-                <Flex.Item>
-                    <Header content="This is your tab" />
-                </Flex.Item>
-                <Flex.Item>
-                    <div>
-                        <div>
-                            <Text content={entityId} />
-                        </div>
+            <Flex column gap="gap.smaller">
+                <Header content="This is your tab" />
+                <Alert icon={<ExclamationTriangleIcon />} content={entityId} dismissible></Alert>
+                <Text content="These are your to-do items:" size="medium"></Text>
+                <List selectable>
+                    {todoItems.map((todoItem, i) => (
+                        <List.Item key={i}
+                            media={<WindowMaximizeIcon outline />}
+                            content={todoItem} index={i} >
+                        </List.Item>))
+                    }
+                </List>
 
-                        <div>
-                            <Button onClick={() => alert("It worked!")}>A sample button</Button>
-                        </div>
-                    </div>
-                </Flex.Item>
-                <Flex.Item styles={{
-                    padding: ".8rem 0 .8rem .5rem"
-                }}>
-                    <Text size="smaller" content="(C) Copyright Codeterian" />
-                </Flex.Item>
+                <Flex gap="gap.medium">
+                    <Flex.Item grow>
+                        <Flex>
+                            <Label icon={<ToDoListIcon />}
+                                styles={{
+                                    background: "darkgray",
+                                    height: "auto",
+                                    padding: "0 15px"
+                                }}></Label>
+                            <Flex.Item grow>
+                                <Input placeholder="New todo item" fluid
+                                    value={newTodoValue}
+                                    onChange={handleOnChanged}></Input>
+                            </Flex.Item>
+                        </Flex>
+                    </Flex.Item>
+                    <Button content="Add Todo" primary
+                        onClick={handleOnClick}></Button>
+                </Flex>
+
+                <Text content="(C) Copyright Contoso" size="smallest"></Text>
             </Flex>
         </Provider>
     );
